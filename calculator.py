b27132fb0482b20c6fbe8e517780deccc7638393
Jitendra Kumar <n8583859@gmail.com>
2026-04-02 22:53:41 +0530

    change errors massages output and update programe code into more manageable parts

--- calculator.py
+++ calculator.py
@@ -2,54 +2,56 @@
 
 
 while True:
-    #Quit or procced 
+    #quit or procced options
     print("\n===========================================================")
     process = input("Click `enter` to porceed or `q` to quit: \n").lower()
-    if (process == "q"):
 
+    #procced or quit
+    if (process == "q"):
         print("\nThanks for aproacohng us! \n Have a good day/night...")
         print("___________________________________________________________")
         break
+
     elif (process == ""):
-        pass
+        #input validation
+        try:
+            #user input for num1 and operator
+            num1 = float(input("Enter first number: "))
+            operator = input("Enter an operator (+,-,*,/): ").strip()
+
+            #handle input wrong operator
+            if operator not in ("+", "-", "*", "/"):
+                print("\n⚠️  Please! Choose operator form (+, -, *, /)\n")
+                continue
+
+            #user input for num2
+            num2 = float(input("Enter second number: ")) 
+            
+        #handle error form user
+        except ValueError as ve:
+            print("\n⚠️  Please! Enter a valid number\n")
+
+        #calculation, result
+        else:
+            if (operator == "+"):
+                    result = num1 + num2
+            elif (operator == "-"):
+                result = num1 - num2
+            elif (operator == "/"):
+                #Handling `ZeroDivisionError`
+                if (num2 == 0):
+                    print("\n⚠️  Division by zero is not defined\n")
+                    continue
+                else:    
+                    result = num1/num2
+            else:
+                result = num1*num2
+
+            #Print result
+            print("\n---------------[RESULT]---------------")
+            print(f"{num1} {operator} {num2} = {result}")
+            print("--------------------------------------\n")
+    
+    #revisit `procced or quit options`
     else:
         continue
-
-    #Input validation
-    try:
-        #User input for num1 and operator
-        num1 = float(input("Enter first number: "))
-        operator = input("Enter an operator (+,-,*,/): ").strip()
-
-        #Handle input wrong operator
-        if operator not in ("+", "-", "*", "/"):
-            print("\nPlease! Choose operator form (+, -, *, /)\n")
-            continue
-
-        #User input for num2
-        num2 = float(input("Enter second number: ")) 
-        
-    #Handle error form user
-    except ValueError as ve:
-        print("\nPlease! Enter a valid number\n")
-
-    #Calculation, result
-    else:
-        if (operator == "+"):
-                result = num1 + num2
-        elif (operator == "-"):
-            result = num1 - num2
-        elif (operator == "/"):
-            #Handling `ZeroDivisionError`
-            if (num2 == 0):
-                print("\nDivision by zero is not defined\n")
-                continue
-            else:    
-                result = num1/num2
-        else:
-            result = num1*num2
-
-        #Print result
-        print("\n---------------[RESULT]---------------")
-        print(f"{num1} {operator} {num2} = {result}")
-        print("--------------------------------------\n")
