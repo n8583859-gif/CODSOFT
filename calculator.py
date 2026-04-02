73e2f9794982c2e040aefadcee6815777f040fb8
Jitendra Kumar <n8583859@gmail.com>
2026-04-02 21:21:45 +0530

    add input validation and handle division by zero

--- calculator.py
+++ calculator.py
@@ -1,23 +1,39 @@
                                     # Calculator with simple functionality
 
-#User input
-num1 = float(input("Enter first number: "))
-operator = input("Choose an operator (+,-,*,/): ").strip()
-num2 = float(input("Enter second number: "))
 
-#Calculation
-if (operator == "+"):
-        result = num1 + num2
-elif (operator == "-"):
-    result = num1 - num2
-elif (operator == "/"):
-    #Handling `ZeroDivisionError`
-    if (num2 == 0):
-         print("Division By Zero Is Not Allowed")
-    else:    
-        result = num1/num2
-else:
-    result = num1*num2
+while True:
+    try:
+        #User input
+        num1 = float(input("Enter first number: "))
+        operator = input("Choose an operator (+,-,*,/): ").strip()
 
-#Print calculated result
-print(f"{num1} {operator} {num2} = {result}")
+        ##Handle wrong operator case
+        if operator not in ("+", "-", "*", "/"):
+            print("Please! Choose operator form (+, -, *, /)")
+            continue
+
+        ##User input
+        num2 = float(input("Enter second number: ")) 
+        
+    #Handle error form user
+    except ValueError as ve:
+        print("Please! Enter a Valid Number")
+
+    else:
+        #Calculation if no error found
+        if (operator == "+"):
+                result = num1 + num2
+        elif (operator == "-"):
+            result = num1 - num2
+        elif (operator == "/"):
+            #Handling `ZeroDivisionError`
+            if (num2 == 0):
+                print("Division By Zero Is Not Allowed")
+                continue
+            else:    
+                result = num1/num2
+        else:
+            result = num1*num2
+
+        #Print calculated result
+        print(f"{num1} {operator} {num2} = {result}")
