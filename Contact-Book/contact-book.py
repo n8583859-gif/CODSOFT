4c99514ba90d7adf5733acd597b4aa0e2e157262
Jitendra Kumar <n8583859@gmail.com>
2026-04-06 17:35:57 +0530

    run program as user's choice and upgrade UI design

--- Contact-Book/contact-book.py
+++ Contact-Book/contact-book.py
@@ -1,10 +1,11 @@
                                  ###...Phone-Book...###
 import re
 
-contact = {}
+contacts = {}
 
+#Options, get user choice
 def get_user_choice():
-    print("|--------[MENU]-------|   ")
+    print("\n|--------[MENU]-------|")
     print("|  1. Add Contact     |")
     print("|  2. View Contacts   |")
     print("|  3. Search Contact  |")
@@ -15,109 +16,135 @@ def get_user_choice():
 
     while True:
         choice = input("Enter your choice (1-6):").strip()
-        try:
-            choice = int(choice)
-            if choice not in range(1,7):
-                raise ValueError
-        except ValueError:
-            print("⚠️  Invalid choice. Please enter a number between 1 and 6.")
+        if choice in ('1', '2', '3', '4', '5', '6'):
+            return choice
+            break
+        else:
+            print("⚠️  Invalid choice. Please enter a number between 1 and 6.\n")
             continue
-        break
 
 
 
+#Validate name
 def get_name():
-    print("--- Add New Contact ---")
-
     while True:
         name = input("Enter name: ").lower()
         if name == '':
-            print("⚠️  Name cannot be empty.")
+            print("⚠️  Name cannot be empty.\n")
             continue
         return name
         break
 
 
-
+#Validate Phone number
 def get_phone_number():
     while True:
         phone_number = input("Enter phone number: ")
         if phone_number.isdigit() and len(phone_number) == 10:
             return phone_number
             break
-        print("⚠️  Invalid phone number. Please enter 10 digits only.")
+        print("⚠️  Invalid phone number. Please enter 10 digits only.\n")
         continue
 
 
-
+#Validate email
 def get_email():
     while True:
         email = input(r"Enter email: ").strip()
-
         pattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
         match = re.match(pattern, email)
-
         if match:
             return email
             break
-        print("⚠️  Invalid email format. Please try again.")
+        print("⚠️  Invalid email format. Please try again.\n")
         continue
 
 
-
+#Validate address
 def get_address():
     address = input("Enter address: ").strip()
     if address == '':
-        print("⚠️  Adderess cannot be empty.")
+        print("⚠️  Adderess cannot be empty.\n")
     return address
    
 
 
+
+#Add contacts into
 def add_contact(name, phone_number, email, address):
-    i = len(contact)+1
-    contact[i] = {'name':name, 'phone_number': phone_number, 'email':email, 'address':address}
+    i = len(contacts)+1
+    contacts[i] = {'name':name, 'phone_number': phone_number, 'email':email, 'address':address}
     print("Contact added successfully.")
 
 
 
-def show_contact_list(contact):
-    if len(contact) > 0:
-        for i in range(1, len(contact)+1):
-            print(f"{i}. {contact[i]['name']}  |   {contact[i]['phone_number']}")
+#Show contact list
+def show_contact_list(contacts):
+    if len(contacts) > 0:
+        for i in range(1, len(contacts)+1):
+            print(f"{i}. {contacts[i]['name']}  |  {contacts[i]['phone_number']}")
     else:
         print("No contacts found.")
 
 
-
-def search_contact(contact):
-    print("--- Search Contact ---")
-
-    search = input("Enter name or phone number:").strip().lower()
-
-    for i in range(1, len(contact)+1):
-        if search == contact[i]['name'] or \
-           search == contact[i]['phone_number']:
-            print(f"name    :  {contact[i]['name']}")
-            print(f"email   :  {contact[i]['phone_number']}")
-            print(f"email   :  {contact[i]['email']}")
-            print(f"address :  {contact[i]['address']}")
+#Search contact via name/phone number
+def search_contact(contacts):
+    if len(contacts) > 0:
+        search = input("Enter name or phone number: ").strip().lower()
+        for i in range(1, len(contacts)+1):
+            if search == contacts[i]['name'] or \
+                search == contacts[i]['phone_number']:
+                print(f"name         :  {contacts[i]['name']}")
+                print(f"phone number :  {contacts[i]['phone_number']}")
+                print(f"emil         :  {contacts[i]['email']}")
+                print(f"address      :  {contacts[i]['address']}")
         else:
-            print("No matching contact found.\n")
+            print("No matching contacts found.")
+    else:
+        print("No contacts found. Please! Add contacts first.")
 
+
+
+# def update_contact():
 
 
 
 def main():
-    print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
     print("           Contact Book")
-    print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
 
     while True:
-        name = get_name()
-        phone_number = get_phone_number()
-        email = get_email()
-        address = get_address()
-        add_contact(name, phone_number, email, address)
+
+        choice = get_user_choice()
+
+        if choice == '1':
+            print("\n_______________[Add New Contact]_______________")
+            name = get_name()
+            phone_number = get_phone_number()
+            email = get_email()
+            address = get_address()
+            add_contact(name, phone_number, email, address)
+            print("_____________________________________________\n")
+            continue
+        elif choice == '2':
+            print("\n__________________[Contacts]___________________")
+            show_contact_list(contacts)
+            print("_____________________________________________\n")
+            continue
+        elif choice == '3':
+            print("\n________________[Search Contact]_______________")
+            search_contact(contacts)
+            print("_______________________________________________\n")
+            continue
+        elif choice == '4':
+            pass
+        elif choice == '5':
+            pass
+        elif choice == '6':
+            print("\n~ See you again!")
+            print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+            break
 
 
 
